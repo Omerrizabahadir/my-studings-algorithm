83d8d314f2ccae701ced47c822e1a448a427e4fa
Omerrizabahadir <omrbahadir@gmail.com>
2024-09-16 18:13:54 +0300

    Algorithm star pattern example added
                         *
                        * *
                       * * *
                      * * * *
                     * * * * *
                    * * * * * *
                   * * * * * * *
                  * * * * * * * *
                 * * * * * * * * *
                * * * * * * * * * *
               * * * * * * * * * * *
              * * * * * * * * * * * *
             * * * * * * * * * * * * *
            * * * * * * * * * * * * * *

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+package src;
+
+public class Main {
+
+    /*
+                     *
+                    * *
+                   * * *
+                  * * * *
+                 * * * * *
+                * * * * * *
+               * * * * * * *
+              * * * * * * * *
+             * * * * * * * * *
+            * * * * * * * * * *
+           * * * * * * * * * * *
+          * * * * * * * * * * * *
+         * * * * * * * * * * * * *
+        * * * * * * * * * * * * * *
+     */
+    public static void main(String[] args) {
+
+        int satirSayisi = 15;
+
+        for (int i = 1; i < satirSayisi; i++) {
+
+            for (int j = 0;j<satirSayisi - i - 1;j++) {
+                System.out.print(" ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
